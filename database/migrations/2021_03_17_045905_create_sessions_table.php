c7d92e685f6badc49b0133339f0ebfeaa25b8b1b
dev kominfosda <dev.kominfosda@gmail.com>
2021-03-30 08:37:50 +0700

    create model migration awal

---
+++ database/migrations/2021_03_17_045905_create_sessions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSessionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sessions', function (Blueprint $table) {
+            $table->id();
+            $table->longText('token')->nullable();
+            $table->string('hostname')->nullable();
+            $table->string('device_id')->nullable();
+            $table->string('status');
+            $table->bigInteger('user_id');
+            $table->timestamp('login_at');
+            $table->timestamp('logout_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sessions');
+    }
+}
