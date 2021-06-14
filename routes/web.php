ccc7d4aac61b68a0b1f3a582af9623896c733fbe
Dev KominfoSDA <dev.kominfosda@gmail.com>
2021-06-14 11:01:11 +0700

    view detail penanganan

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,8 @@ Route::post('/logout', 'Auth\LoginController@logout')->name('logout');
 Route::get('/register', 'Auth\RegisterController@showRegistrationForm')->name('register');
 Route::post('register', 'Auth\RegisterController@register')->name('register');
 
-Route::middleware(['auth'])->group(
-    function () {
-        Route::resources([
-            'dashboard' => 'DashboardsController',
+Route::resources([
+    'dashboard' => 'DashboardsController',
             'detail_penanganan' => 'DetailHandlingsController',
             'input_kejadian' => 'InputIncidentsController',
             'req_bantuan' => 'RequestAssistsController',
@@ -45,6 +43,42 @@ Route::middleware(['auth'])->group(
             'pasien' => 'PatientDatasController',
             'task' => 'TasksController',
             'peta' => 'ShowMapsController'
+]);
+
+Route::middleware(['auth'])->group(
+    function () {
+        Route::resources([
+            // 'dashboard' => 'DashboardsController',
+            // 'detail_penanganan' => 'DetailHandlingsController',
+            // 'input_kejadian' => 'InputIncidentsController',
+            // 'req_bantuan' => 'RequestAssistsController',
+            // 'edit_laporan' => 'EditReportsController',
+            // 'edit_kejadian' => 'EditIncidentsController',
+            // 'konfirmasi_penugasan' => 'AssignmentConfirmationsController',
+            // 'rekap_shift' => 'RecapShiftsController',
+            // 'monitoring_alarm' => 'MonitoringAlarmsController',
+            // 'penanganan_kejadian' => 'IncidentHandlingsController',
+            // 'monitoring_resources' => 'MonitoringResourcesController',
+            // 'laporan_kejadian' => 'ReportIncidentsController',
+            // 'rekap_kejadian' => 'RecapIncidentsController',
+            // 'rekap_bantuan' => 'RecapAssistsController',
+            // 'rekap_penolakan_pembatalan' => 'RecapRejectionCancellationsController',
+            // 'rekap_perawat' => 'RecapNursesController',
+            // 'rekap_ambulan_offline' => 'RecapAmbulancesController',
+            // 'personil' => 'PersonsController',
+            // 'management_user' => 'UsersController',
+            // 'devicelist' => 'DevicesController',
+            // 'emergency_button_user' => 'EmergencyButtonUsersController',
+            // 'faskes' => 'HealthFacilitiesController',
+            // 'ambulan' => 'ResourcesController',
+            // 'spesialisasi_dokter' => 'SpecialistsController',
+            // 'kategori' => 'CategoriesController',
+            // 'sub_kategori' => 'SubCategoriesController',
+            // 'obat' => 'MedicinesController',
+            // 'shift' => 'RshiftsController',
+            // 'pasien' => 'PatientDatasController',
+            // 'task' => 'TasksController',
+            // 'peta' => 'ShowMapsController'
         ]);
         Route::get('get-incidents', 'RequestAssistsController@getIncidents')->name('get-incidents');
         Route::get('get-incident/{id}', 'RequestAssistsController@getIncident');
