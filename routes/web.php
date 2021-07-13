1c2a557e296382d57df8d750f8c09b229f23e6a2
Dev KominfoSDA <dev.kominfosda@gmail.com>
2021-07-13 15:38:14 +0700

    perbaikan route rshift yang error datatable tidak muncul

--- routes/web.php
+++ routes/web.php
@@ -85,8 +85,8 @@ Route::middleware(['auth'])->group(
         Route::get('get-layanan', 'SpecialistsController@getLayanan');
         Route::get('get-task', 'TasksController@getTask');
         Route::get('get-form-data-category', 'CategoriesController@getFormDataCategory')->name('get-form-data-category');
-        Route::get('get-rshift', 'RShiftsController@getRShift');
-        Route::get('get-form-data-healthfacility', 'RShiftsController@getFormDataHealthFacility')->name('get-form-data-healthfacility');
-        Route::get('get-form-data-resource/{id}', 'RShiftsController@getFormDataResource')->name('get-form-data-resource');
+        Route::get('get-rshift', 'RshiftsController@getRShift');
+        Route::get('get-form-data-healthfacility', 'RshiftsController@getFormDataHealthFacility')->name('get-form-data-healthfacility');
+        Route::get('get-form-data-resource/{id}', 'RshiftsController@getFormDataResource')->name('get-form-data-resource');
     }
 );
